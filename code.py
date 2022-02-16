e73cf25fed1c663b5881be81bf55c48ac399da24
rivakh <83762049+rivakh@users.noreply.github.com>
2022-02-16 23:21:36 +0000

    code to create pages from slides

---
+++ code.py
@@ -0,0 +1,43 @@
+# thumbnail 70/100
+# large 1657 × 2366
+# normal 
+
+import os
+import sys
+from turtle import right
+import cv2
+
+
+def resize_thumb(img,i):
+    scale_percent = 10 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+  
+    # resize image
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    print(resized.shape)
+    if i:
+        cv2.imwrite(os.path.join(sys.argv[2], f'{i}-thumb'+ '.jpg'), resized)
+        cv2.imwrite(os.path.join(sys.argv[2], f'{i}-large'+ '.jpg'), img)
+        cv2.imwrite(os.path.join(sys.argv[2], f'{i}'+ '.jpg'), img)
+
+def main():
+    os.makedirs(sys.argv[2], exist_ok=True)
+    images = sorted(os.listdir(sys.argv[1]), key=lambda x: int(x[5:-4]))
+    i = 0
+    for image in images:
+        img = cv2.imread(os.path.join(sys.argv[1], image))
+        height, width, channels = img.shape
+        left_image, right_image = img[:, :width//2, :], img[:, width//2:, :]
+        print(left_image.shape, right_image.shape , img.shape)
+        resize_thumb(left_image,i)
+        i += 1
+        resize_thumb(right_image,i)
+        i+=1
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
