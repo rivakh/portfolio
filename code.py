17fdd1c3eeac7012e8cf8c3386c5de2f6dd062c1
shekharkoirala <shekharkoirala4@gmail.com>
2022-05-17 01:11:15 +0100

    added thesis

--- code.py
+++ code.py
@@ -8,6 +8,24 @@ from turtle import right
 import cv2
 
 
+def resize_thumb_thesis(img,i):
+    scale_percent = 10 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+  
+    # resize image
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    scale_percent = 100 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    maini = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    print(resized.shape)
+    cv2.imwrite(os.path.join(sys.argv[2], f'{i}-thumb'+ '.jpg'), resized)
+    cv2.imwrite(os.path.join(sys.argv[2], f'{i}-large'+ '.jpg'), img)
+    cv2.imwrite(os.path.join(sys.argv[2], f'{i}'+ '.jpg'), maini)
+
 def resize_thumb(img,i):
     scale_percent = 50 # percent of original size
     width = int(img.shape[1] * scale_percent / 100)
@@ -30,12 +48,15 @@ def main():
     for image in images:
         img = cv2.imread(os.path.join(sys.argv[1], image))
         height, width, channels = img.shape
-        left_image, right_image = img[:, :width//2, :], img[:, width//2:, :]
-        print(left_image.shape, right_image.shape , img.shape)
-        resize_thumb(left_image,i)
+        resize_thumb_thesis(img,i+1)
         i += 1
-        resize_thumb(right_image,i)
-        i+=1
+        # left_image, right_image = img[:, :width//2, :], img[:, width//2:, :]
+        # print(left_image.shape, right_image.shape , img.shape)
+        
+        # resize_thumb(left_image,i)
+        # i += 1
+        # resize_thumb(right_image,i)
+        # i+=1
     pass
 
 
